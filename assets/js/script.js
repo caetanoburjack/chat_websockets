b371e96d293c46092f524e7e19c7af96e18e12a9
Caetano Burjack <caetano.burjack@gmail.com>
2023-05-09 14:17:15 +0100

    Uploading some updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,137 @@
-var form1 = document.getElementById('form1');
+var sendMessageForm = document.getElementById('sendMessageForm');
+var newRoomForm = document.getElementById('newRoomForm');
+var changeNameForm = document.getElementById('changeNameForm');
 var content = document.getElementById('content');
-var divSalas = document.getElementById('salas');
+var divSalas = document.getElementById('rooms');
+var roomTitle = document.getElementById('roomTitle');
+var spanUserName = document.getElementById('spanUserName');
 var conn;
 var conn_status = false;
-window.onload = connect;
 
-form1.addEventListener('submit', function (e) {
+window.onload = initizile;
+
+function initizile() {
+    mostrarSalas();
+    showUserName();
+}
+
+function showUserName() {
+    var userName = localStorage.getItem('chat_username');
+    spanUserName.innerHTML = userName;
+    console.log('Now your username is: ' + userName);
+}
+
+newRoomForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var roomName = document.getElementById('roomName').value;
+    var userName = document.getElementById('userNameOnRoomForm').value;
+    criarSala(roomName);
+    saveUserName(userName);
+    document.getElementById('roomName').value = '';
+    document.getElementById('newRoomForm').style.display = 'none';
+    localStorage.setItem('chat_current_room', roomName);
+    console.log('Available Rooms: ' + localStorage.getItem('chat_rooms'))
+    console.log('Current Room: ' + localStorage.getItem('chat_current_room'))
+    connect();
+});
+
+changeNameForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    var name = localStorage.getItem('chat_usuario') ? localStorage.getItem('chat_usuario') : document.getElementById('name').value;
-    var room = document.getElementById('room').value;
+    var userName = document.getElementById('userName').value;
+    saveUserName(userName);
+    document.getElementById('userName').value = '';
+    document.getElementById('changeNameForm').style.display = 'none';
+});
+
+sendMessageForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var name = localStorage.getItem('chat_username');
+    var room = localStorage.getItem('chat_current_room');
     var message = document.getElementById('message').value;
-    localStorage.setItem('chat_sala_atual', room);
-    localStorage.setItem('chat_usuario', name);
-    //criarSala(room);
 
     if (name && message) {
-        var data = {'name': name, 'message': message};
+
+        $msgTime = getRightTime();
+        var data = {
+            'name': name, 'message': message, 'msgTime': $msgTime
+        };
 
         conn.publish(room, data);
     }
-    console.log('Salas existentes: ' + localStorage.getItem('chat_salas'))
-    console.log('A sala atual eh: ' + localStorage.getItem('chat_sala_atual'))
+    document.getElementById('message').value = '';
+    document.getElementById('message').focus();
+    console.log('Available Rooms: ' + localStorage.getItem('chat_rooms'))
+    console.log('Current Room: ' + localStorage.getItem('chat_current_room'))
 });
 
+function getRightTime() {
+    var msgTime = new Date();
+    var dia = msgTime.getDate();               // 1-31
+    var dia_sem = msgTime.getDay();            // 0-6 (zero=sunday)
+    var mes = msgTime.getMonth();              // 0-11 (zero=january)
+    var ano2 = msgTime.getYear();                      // 2 digits
+    var ano4 = msgTime.getFullYear();          // 4 digits
+    var hora = msgTime.getHours();             // 0-23
+    var min = msgTime.getMinutes();            // 0-59
+    var seg = msgTime.getSeconds();            // 0-59
+    var mseg = msgTime.getMilliseconds();      // 0-999
+    var tz = msgTime.getTimezoneOffset();      // in minutes
 
-form2.addEventListener('submit', function (e) {
-    e.preventDefault();
+    var str_data = (mes + 1) + '/' + dia + '/' + ano4;
+    var str_hora = hora + ':' + min + ':' + seg;
 
-    var name = localStorage.getItem('chat_usuario') ? localStorage.getItem('chat_usuario') : document.getElementById('name').value;
-    var room = localStorage.getItem('chat_sala_atual');
-    var message = document.getElementById('mensagem').value;
-
-    if (name && message) {
-        var data = {'name': name, 'message': message};
-
-        conn.publish(room, data);
-    }
-    document.getElementById('mensagem').value = '';
-    document.getElementById('mensagem').focus();
-    console.log("Altua atual da div: " + content.scrollHeight);
-    console.log('Salas existentes: ' + localStorage.getItem('chat_salas'))
-    console.log('A sala atual eh: ' + localStorage.getItem('chat_sala_atual'))
-});
+    return str_data + ' ' + str_hora;
+}
 
 // function mostrarDados() {
 //     document.getElementById("dados").innerHTML = "Full Height: " + content.scrollHeight.toFixed() + "<br>Horizontally: " + content.scrollLeft.toFixed() + "<br>Vertically: " + content.scrollTop.toFixed();
 // }
 
-function criarSala(sala) {
-    var salas = localStorage.getItem('chat_salas');
-    if (salas) {
-        var arraySalas = JSON.parse(salas);
-        if (arraySalas.indexOf(sala) == -1) {
-            arraySalas.push(sala);
+function criarSala(room) {
+    var rooms = localStorage.getItem('chat_rooms');
+    if (rooms) {
+        var arraySalas = JSON.parse(rooms);
+        if (arraySalas.indexOf(room) == -1) {
+            arraySalas.push(room);
             var arraySalasJson = JSON.stringify(arraySalas);
-            localStorage.setItem('chat_salas', arraySalasJson);
+            localStorage.setItem('chat_rooms', arraySalasJson);
         } else {
-            console.log("A sala " + sala + " já existe!");
+            console.log("The room " + room + " already exists!");
         }
     } else {
-        var arraySalas = [sala];
+        var arraySalas = [room];
         var arraySalasJson = JSON.stringify(arraySalas);
-        localStorage.setItem('chat_salas', arraySalasJson);
+        localStorage.setItem('chat_rooms', arraySalasJson);
     }
-    tornarSalaAtual(sala);
+    tornarSalaAtual(room);
 }
 
-function tornarSalaAtual(sala) {
-    localStorage.setItem('chat_sala_atual', sala);
-    console.log('Agora a sala atual é a ' + sala);
+function tornarSalaAtual(room) {
+    localStorage.setItem('chat_current_room', room);
+    console.log('Now the current room is: ' + room);
+}
+
+function saveUserName(userName) {
+    localStorage.setItem('chat_username', userName);
+    showUserName();
 }
 
 function connect() {
-    var room = document.getElementById('room').value;
+    var room = localStorage.getItem('chat_current_room') ? localStorage.getItem('chat_current_room') : 'room';
     if (conn_status) {
         conn.close();
         conn_status = false;
         content.innerHTML = '';
     }
-    console.log('Se conectando a ' + room);
+    console.log('Connecting to ' + room);
     conn = new ab.Session('ws://chat.localhost:8080', function () {
         conn_status = true;
         tornarSalaAtual(room);
         conn.subscribe(room, function (topic, data) {
-            console.log('Conectado a ' + room);
-            console.log('Mensagens da sala ' + room + ': ' + data);
+            console.log('Conected to ' + room);
+            console.log('Messages of ' + room + ': ' + data);
             if (typeof data === 'string') {
                 data = JSON.parse(data);
 
@@ -100,29 +146,37 @@ function connect() {
         console.warn('WebSocket connection closed');
     }, {'skipSubprotocolCheck': true});
     mostrarSalas();
-
+    mostrarNomeSala(room);
 }
 
-function mostrarSalas(data) {
-    var salasArmazenadas = localStorage.getItem('chat_salas');
-    salasArmazenadas = JSON.parse(salasArmazenadas);
-    for (var i = 0; i < salasArmazenadas.length; i++) {
-        var p = document.createElement('p');
-        p.setAttribute('class', 'cada_sala');
-        p.textContent = salasArmazenadas[i];
-        divSalas.appendChild(p);
+function mostrarSalas() {
+    var roomsArmazenadas = localStorage.getItem('chat_rooms');
+    if (roomsArmazenadas) {
+        roomsArmazenadas = JSON.parse(roomsArmazenadas);
+        divSalas.innerHTML = '';
+        for (var i = 0; i < roomsArmazenadas.length; i++) {
+            var p = document.createElement('p');
+            p.setAttribute('class', 'each_room');
+            p.setAttribute('onclick', 'activateRoom("' + roomsArmazenadas[i] + '")');
+            p.textContent = roomsArmazenadas[i];
+            divSalas.appendChild(p);
+        }
     }
+}
+
+function mostrarNomeSala(room) {
+    roomTitle.innerHTML = room;
 }
 
 //Printar Mensagens na Tela
 function showMessages(data) {
-    if (data.name == 'rafa') {
-        var img_src = "assets/imgs/user.png";
+    if (data.name == 'Caetano') {
+        var img_src = "assets/img/user.png";
     } else if (data.name == 'bruno') {
-        var img_src = "assets/imgs/user2.png";
+        var img_src = "assets/img/user2.png";
     }
 
-    var salasArmazenadas = localStorage.getItem('chat_salas')
+    var roomsArmazenadas = localStorage.getItem('chat_rooms')
 
     var div = document.createElement('div');
     div.setAttribute('class', 'me');
@@ -133,8 +187,13 @@ function showMessages(data) {
     var div_txt = document.createElement('div');
     div_txt.setAttribute('class', 'text');
 
+    var span = document.createElement('span');
+    span.setAttribute('class', 'msgTime');
+    span.textContent = data.msgTime;
+
     var h5 = document.createElement('h5');
     h5.textContent = data.name;
+    h5.appendChild(span);
 
     var p = document.createElement('p');
     p.textContent = data.message;
@@ -146,6 +205,24 @@ function showMessages(data) {
     div.appendChild(div_txt);
 
     content.appendChild(div);
-    content.scrollTop = content.scrollHeight - 705;
+    content.scrollTop = content.scrollHeight;
 
+}
+
+function showNewRoomForm() {
+    if (!localStorage.getItem('chat_username')) {
+        document.getElementById('userNameOnRoomForm').style.display = 'inline-block'
+    }
+    document.getElementById('newRoomForm').style.display = 'inline-block';
+}
+
+function showChangeNameForm() {
+    document.getElementById('changeNameForm').style.display = 'block';
+}
+
+function activateRoom(room) {
+    localStorage.setItem('chat_current_room', room);
+    console.log('Available Rooms: ' + localStorage.getItem('chat_rooms'))
+    console.log('Current Room: ' + localStorage.getItem('chat_current_room'))
+    connect();
 }
--- index.php
+++ index.php
@@ -5,46 +5,45 @@
     <title>Chat</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <style>
-        .divInputMessage {
-            width: 100%;
-            height: 0px;
-            background: #232F48;
-            z-index: 9999;
-        }
-    </style>
 </head>
 <body>
-<div class="container" style="display: flex; flex-direction: row">
-    <div class="colunaEsquerda">
+<div class="container">
+    <div class="leftColumn">
         <img src="assets/img/chat.png" alt="Chat" title="Chat"/>
-        <div class="salas" id="salas">
-
-        </div>
-        <form id="form1">
-            <input type="text" id="room" name="room" placeholder="Sala" onfocusout="connect()">
-            <input type="text" id="name" placeholder="Nome">
-            <input type="text" id="message">
-            <button id="btn1">Entrar na Sala</button>
+        <button class="defaultBtn" onclick="showNewRoomForm()" id="newRoom">New Room</button>
+        <form id="newRoomForm" style="display: none">
+            <input type="text" id="roomName" name="roomName" placeholder="Room Name">
+            <input type="text" id="userNameOnRoomForm" name="userName" placeholder="User Name" style="display: none">
+            <button class="defaultBtn" id="btnCreateRoom">Create Room</button>
         </form>
-    </div>
-    <div class="colunaDireita"
-         style="display: flex; flex-direction: column; ">
-        <div id="content"
-             style="background: pink;display: flex; flex-direction: column; overflow-x: auto;">
 
+        <div class="rooms" id="rooms"></div>
+        <form id="changeNameForm" style="display: none">
+            <input type="text" id="userName" name="userName" placeholder="User Name">
+            <button class="defaultBtn" id="btnCreateRoom">Save User Name</button>
+        </form>
+        <div id="divUserName">
+            <span id="spanUserName"></span>
+            <button class="defaultBtn" onclick="showChangeNameForm()" id="changeName">Change User
+                Name
+            </button>
         </div>
+    </div>
+
+    <div class="rightColumn">
+        <div id="roomTitle"></div>
+        <div id="content"></div>
         <div class="divInputMessage">
-            <form id="form2">
-                <input type="text" id="mensagem"/>
-                <button type="submit">Enviar</button>
+            <form id="sendMessageForm">
+                <input type="text" class="inputMessage" autocomplete="false" id="message"/>
             </form>
         </div>
-
     </div>
+
 </div>
 
 <script src="assets/js/autobahn.js"></script>
 <script src="assets/js/script.js"></script>
+
 </body>
 </html>
